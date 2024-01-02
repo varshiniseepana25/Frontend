811861cbfef543eae6cf1b4b4e81f2e5003fbb6d
varshiniseepana25 <varshiniseepana@gmail.com>
2024-01-02 18:48:00 +0530

    firstCommit

---
+++ Cat/cat.js
@@ -0,0 +1,20 @@
+function SwitchOff() {
+    document.getElementById("bulbimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent="Switched Off";
+    document.getElementById("onSwitch").style.backgroundColor="#22c55e";
+    document.getElementById("offSwitch").style.backgroundColor="#cbd2d9";
+
+
+}
+
+function SwitchOn() {
+    document.getElementById("bulbimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catimage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent="Switched On";
+    document.getElementById("offSwitch").style.backgroundColor="#e12d39";
+    document.getElementById("onSwitch").style.backgroundColor="#cbd2d9";
+    
+
+
+}
